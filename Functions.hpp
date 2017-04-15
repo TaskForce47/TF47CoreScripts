725d5e45331ae567be4938ac6bfea72a8b3f295f
Niner1309 <sven0215@hotmail.com>
2017-04-15 15:09:51 +0200

    First commit

---
+++ Functions.hpp
@@ -0,0 +1,8 @@
+class tf47_core
+{
+    class tf47_core
+    {
+        file = "TF47CoreScripts";
+        class init {};
+    };
+};
---
+++ configMacros.hpp
@@ -0,0 +1,15 @@
+
+
+
+#define AIR 1
+#define TANK 2
+#define CCT 3
+
+#define ON true
+#define OFF false
+
+#define WHITELIST(STATE)  tf47_core_whitelistActive = STATE; publicVariable "tf47_core_whitelistActive";
+#define WHITELIST_SYSTEM(STATE) tf47_core_whitelistSystem = STATE; publicVariable "tf47_core_whitelistSystem";
+
+
+
